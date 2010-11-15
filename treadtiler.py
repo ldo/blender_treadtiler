f957161ee8d5c20da7e1211dd3ee804059c911ab
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2010-11-15 08:35:06 +0000

    call this 0.1.1

--- treadtiler.py
+++ treadtiler.py
@@ -25,7 +25,7 @@ bl_addon_info = \
     {
         "name" : "Tread Tiler",
         "author" : "Lawrence D'Oliveiro <ldo@geek-central.gen.nz>",
-        "version" : (0, 1, 0),
+        "version" : (0, 1, 1),
         "blender" : (2, 5, 5),
         "api" : 32411,
         "location" : "View 3D > Edit Mode > Tool Shelf",
